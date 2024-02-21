b8d3746e4967f229e611efa6f6d332a24ffecacf
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-02-22 00:20:58 +0300

    Add active users & Enhance collaboration features
    Majority of the collaboration features are done, app can be used as a base for future projects at this point.

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,16 @@
 "use client";
 
 import Live from "@/components/Live";
+import Navbar from "@/components/Navbar";
 
 export default function Page() {
   return (
-    <div>
-      <Live />
-    </div>
+    <main className="h-screen overflow-hidden">
+      <Navbar />
+      <section className="flex h-full flex-row">
+        <Live />
+      </section>
+    </main>
   );
 
   //Next.js renders all components at server side by default
