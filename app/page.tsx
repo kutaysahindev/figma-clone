677b6b2173587a36e6b8fd2c4821800b0c8fc163
Kutay Şahin <79334889+kutaysahindev@users.noreply.github.com>
2024-02-22 22:44:53 +0300

    Overall component enhancements

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,66 @@
 "use client";
 
+import { fabric } from "fabric";
+
+import LeftSidebar from "@/components/LeftSidebar";
 import Live from "@/components/Live";
 import Navbar from "@/components/Navbar";
+import RightSidebar from "@/components/RightSidebar";
+import { useEffect, useRef, useState } from "react";
+import {
+  handleCanvasMouseDown,
+  handleResize,
+  initializeFabric,
+} from "@/lib/canvas";
+import { ActiveElement } from "@/types/type";
 
 export default function Page() {
+  const canvasRef = useRef<HTMLCanvasElement>(null);
+  const fabricRef = useRef<fabric.Canvas | null>(null);
+  const isDrawing = useRef(false);
+  const shapeRef = useRef<fabric.Object | null>(null); //Reference to  shape that user is currently drawing
+  const selectedShapeRef = useRef<string | null>("rectangle"); //Reference to shape that user has selected
+
+  const [activeElement, setActiveElement] = useState<ActiveElement>({
+    name: "",
+    value: "",
+    icon: "",
+  });
+
+  const handleActiveElement = (elem: ActiveElement) => {
+    setActiveElement(elem);
+
+    selectedShapeRef.current = elem?.value as string;
+  };
+
+  useEffect(() => {
+    const canvas = initializeFabric({ canvasRef, fabricRef });
+
+    canvas.on("mouse:down", (options) => {
+      handleCanvasMouseDown({
+        options,
+        canvas,
+        isDrawing,
+        shapeRef,
+        selectedShapeRef,
+      });
+    });
+
+    window.addEventListener("resize", () => {
+      handleResize({ fabricRef });
+    });
+  }, []);
+
   return (
     <main className="h-screen overflow-hidden">
-      <Navbar />
+      <Navbar
+        activeElement={activeElement}
+        handleActiveElement={handleActiveElement}
+      />
       <section className="flex h-full flex-row">
-        <Live />
+        <LeftSidebar />
+        <Live canvasRef={canvasRef} />
+        <RightSidebar />
       </section>
     </main>
   );
--- components/Live.tsx
+++ components/Live.tsx
@@ -14,7 +14,11 @@ import ReactionSelector from "./reaction/ReactionButton";
 import FlyingReaction from "./reaction/FlyingReaction";
 import useInterval from "@/hooks/useInterval";
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
   //--> State Def.
   const others = useOthers(); //From Liveblocks, returns a list of all other users in the same room
 
@@ -166,13 +170,14 @@ const Live = () => {
 
   return (
     <div
+      id="canvas"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+      <canvas ref={canvasRef} />
 
       {reaction.map((r) => (
         <FlyingReaction
